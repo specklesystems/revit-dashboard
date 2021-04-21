f0a93df9cee35c49b77cac0b62bfad751fe40076
Alan Rynne <alan@speckle.systems>
2021-04-21 14:45:53 +0200

    Added vuetify with same config as frontend

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
@@ -10,5 +11,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
