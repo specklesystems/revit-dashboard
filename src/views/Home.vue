862cd19c8a52add8934cac10bce776a69c0fd884
Alan Rynne <alan@speckle.systems>
2021-04-21 13:41:04 +0200

    Added url, removed apollo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <button @click="this.authRedirect">Login with Speckle</button>
     <div id="viewer" ref="speckle-viewer"></div>
   </div>
 </template>
@@ -13,14 +14,17 @@ export default {
   async mounted() {
     this.viewer = new Viewer({ container: this.$refs["speckle-viewer"] })
     await this.viewer.loadObject(this.url, this.token)
-    console.log(this.viewer.interactions)
-    this.viewer.interactions.selectionHelper.on('object-clicked', (o) => { console.log("object clicked", o)})
   },
   data: () => {
     return {
       viewer: null,
       token: "d7fddb61b5820d92ba1342d1981c8411b1fee500be",
       url: "https://speckle.xyz/streams/646cab6494/objects/550a2ef6d20e7f88853388b8d60de6c3"
+    }
+  },
+  methods: {
+    authRedirect: () => {
+      window.location = "https://speckle.xyz/authn/verify/de9e84c7ca/myChallenge"
     }
   }
 }
